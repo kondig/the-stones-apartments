27bbbdb29e14100189f7e07dac5bacd32f70f56e
kondig <digonisk@gmail.com>
2020-08-29 16:28:12 +0300

    1st deployment + s-m mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,34 @@ function Footer() {
         <div className="App-footer">
             <h2>Contact</h2>
             <div className="socialmedia">
-                <a href="https://www.facebook.com/apartmentsthestones" target="_blank" rel="noopener noreferrer" className="link" >
-                  <Tooltip title='facebook' placement="bottom">
-                    <i className='fa fa-facebook faicon fablack'  />
-                  </Tooltip>
-                </a>
-                <a href="https://www.instagram.com/apartmentsthestones/" target="_blank" rel="noopener noreferrer" className="link" >
-                  <Tooltip title='instagram' placement="bottom">
-                    <i className='fa fa-instagram faicon fablack' />
-                  </Tooltip>
-                </a>
-                <a href="mailto:giampourask@gmail.com" target="_blank" rel="noopener noreferrer" className="link" >
-                  <Tooltip title='email: giampourask@gmail.com' placement="bottom">
-                    <i className='fa fa-paper-plane faicon fablack' />
-                  </Tooltip>
-                </a>
-                <a href="tel:00306944720770" target="_blank" rel="noopener noreferrer" className="link" >
-                  <Tooltip title='+30 69 44 72 07 70' placement="bottom">
-                    <i className='fa fa-phone faicon fablack' />
-                  </Tooltip>
-                </a>
+                <div className="sm-item">
+                    <a href="https://www.facebook.com/apartmentsthestones" target="_blank" rel="noopener noreferrer" className="link" >
+                      <Tooltip title='facebook' placement="bottom">
+                        <i className='fa fa-facebook faicon fablack'  />
+                      </Tooltip>
+                    </a>
+                </div>
+                <div className="sm-item">
+                    <a href="https://www.instagram.com/apartmentsthestones/" target="_blank" rel="noopener noreferrer" className="link" >
+                      <Tooltip title='instagram' placement="bottom">
+                        <i className='fa fa-instagram faicon fablack' />
+                      </Tooltip>
+                    </a>
+                </div>
+                <div className="sm-item">
+                    <a href="mailto:giampourask@gmail.com" target="_blank" rel="noopener noreferrer" className="link" >
+                      <Tooltip title='email: giampourask@gmail.com' placement="bottom">
+                        <i className='fa fa-paper-plane faicon fablack' />
+                      </Tooltip>
+                    </a>
+                </div>
+                <div className="sm-item">
+                    <a href="tel:00306944720770" target="_blank" rel="noopener noreferrer" className="link" >
+                      <Tooltip title='+30 69 44 72 07 70' placement="bottom">
+                        <i className='fa fa-phone faicon fablack' />
+                      </Tooltip>
+                    </a>
+                </div>
             </div>
             <div className="pts-container">
                 <div className="name-address">
