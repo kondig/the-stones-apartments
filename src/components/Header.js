cbf36ff85a5842bb81d2bc34cd55c0526354bf24
kondig <digonisk@gmail.com>
2020-08-29 22:43:44 +0300

    header bg and content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import reactlogo from '../logo.svg';
 import logo from '../images/ats-logo.png';
-import sketch from '../images/ats-sketch.jpg';
+import sketch from '../images/ats-sketch.png';
 import Tooltip from '@material-ui/core/Tooltip';
 
 
@@ -9,10 +9,10 @@ function Header() {
     return (
         <header className="App-header">
           <div className="row">
+            <p className="heading">Apartments <br/> The Stones</p>
             <img src={sketch} className="ats-sketch" alt="logo" />
           </div>
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="socialmedia">
+          <div className="socialmedia p-m">
               <a href="https://www.facebook.com/apartmentsthestones" target="_blank" rel="noopener noreferrer" className="link" >
                 <Tooltip title='facebook' placement="bottom">
                   <i className='fa fa-facebook faicon'  />
@@ -24,6 +24,8 @@ function Header() {
                 </Tooltip>
               </a>
           </div>
+          {/* <img src={logo} className="App-logo" alt="logo" />*/}
+
         </header>
     )
 }
