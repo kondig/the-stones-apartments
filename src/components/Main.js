3866d041f1dff045e997c713258a05dee88a53cc
kondig <digonisk@gmail.com>
2020-08-29 19:44:37 +0300

    main section -v1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import emery from '../images/emery-rock.jpg';
+import lime from '../images/limestone-rock.jpg';
+import Tooltip from '@material-ui/core/Tooltip';
+import {Apartment} from './stone.js'
 
 function Main() {
     return (
         <div className="App-main">
-            <h2> Coming <br/> Soon </h2>
-
+            <p className="text">
+                Apartments The Stones is a hospitality service.
+                The contribution in creating unforgettable memories in your vacation time.
+                <br/><br/>
+                We offer a nice place to stay and tailor made recommendations regarding food & drinks, activities and sightseeing in order to complete your Naxos experience.
+            </p>
+            <div className="apartments">
+                <div className="ap-item">
+                    <Apartment title='Emery Stone' subheader='Stelida, Naxos' imgfile={emery} imgtitle='Emery Stone' link='https://www.airbnb.com/rooms/2843216?guests=1&adults=1' />
+                </div>
+                <div className="ap-item">
+                    <Apartment title='Limestone' subheader='Chora, Naxos' imgfile={lime} imgtitle='Limestone' link='https://www.airbnb.com/rooms/23591413?guests=1&adults=1' />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/stone.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAirbnb } from '@fortawesome/free-brands-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 500,
+    width: 350,
+    borderBottomRightRadius:0,
+    borderBottomLeftRadius:0,
+    backgroundColor: '#000000',
+    fontFamily: 'Open Sans, sans-serif',
+    [theme.breakpoints.down('sm')]: {
+      width: 170
+    },
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  content: {
+      fontFamily: 'Open Sans, sans-serif',
+      color:'#ffffff'
+  },
+  cardactions: {
+      display:'flex',
+      justifyContent:'center',
+      backgroundColor: '#000000'
+  },
+}));
+
+function Stone(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+        <a href={props.link} target="_blank" rel="noopener noreferrer" className="link" >
+            <CardMedia
+              className={classes.media}
+              image={props.imgfile}
+              title={props.imgtitle}
+            />
+        </a>
+
+      <CardHeader className={classes.content}
+        title={props.title}
+        subheader={props.subheader}
+      />
+      {/* <CardContent className={classes.content}>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.descMain}
+        </Typography>
+      </CardContent>*/}
+      <CardActions disableSpacing className={classes.cardactions}>
+          <a href={props.link} target="_blank" rel="noopener noreferrer" className="link" >
+            <FontAwesomeIcon icon={faAirbnb} color="#ffffff" size="2x" />
+          </a>
+      </CardActions>
+    </Card>
+  );
+}
+
+class Apartment extends React.Component {
+  constructor(props) {
+      super(props)
+  }
+	render() {
+    const { imgfile, imgtitle, title, descMain, link, subheader } = this.props;
+		return (
+			<div>
+				<Stone
+                  imgfile = { imgfile }
+                  imgtitle = { imgtitle }
+                  title = { title }
+                  subheader = { subheader }
+                  descMain = { descMain }
+                  link = { link }
+                 />
+			</div>
+		);
+	}
+}
+
+export {Apartment};
